a3d0be66c61556eb2e4ba81196d726d7c6362073
brunosimon <simon.bruno.77@gmail.com>
2026-02-22 12:27:54 +0100

    :sparkles: Scenery > Road > Put back glitters

--- sources/Game/World/Scenery.js
+++ sources/Game/World/Scenery.js
@@ -1,4 +1,4 @@
-import { color, Fn, max, PI, positionWorld, texture, uniform, uv, vec3 } from 'three/tsl'
+import { color, float, Fn, max, PI, positionWorld, texture, uniform, uv, vec3 } from 'three/tsl'
 import { Game } from '../Game.js'
 import { References } from '../References.js'
 import { MeshDefaultMaterial } from '../Materials/MeshDefaultMaterial.js'
@@ -35,6 +35,11 @@ export class Scenery
         }
 
         this.setRoad()
+
+        this.game.ticker.events.on('tick', () =>
+        {
+            this.update()
+        })
     }
     
     setRoad()
@@ -45,23 +50,39 @@ export class Scenery
         const mesh = this.references.items.get('road')[0]
         
         this.road.color = uniform(color('#383039'))
-        this.road.glitterScarcity = uniform(0.1)
-        this.road.glitterLighten = uniform(0.28)
         this.road.middleLighten = uniform(0.1)
+        this.road.glitterViewMultiplier = 0.01
+        this.road.glitterVariation = uniform(0)
+        this.road.glitterScarcity = uniform(100)
+        this.road.glitterIntensity = uniform(0.3)
+        this.road.glitterPerlinFrequency = uniform(0.05)
+        this.road.glitterHashFrequency = uniform(0.2)
 
         const colorNode = Fn(() =>
         {
-            const glitterUv = positionWorld.xz.mul(0.2)
-            const glitter = texture(this.game.noises.hash, glitterUv).r
-            
-            const glitterLighten = glitter.remap(this.road.glitterScarcity.oneMinus(), 1, 0, this.road.glitterLighten)
+            const glitter = float(0)
 
-            // return vec3(glitterLighten)
-            
-            const middleLighten = uv().y.mul(PI).sin().mul(this.road.middleLighten)
+            // Hash
+            const hashUv = positionWorld.xz.mul(this.road.glitterHashFrequency)
+            const hash = texture(this.game.noises.hash, hashUv).r.mul(2).add(this.road.glitterVariation).mod(2).sub(1).abs()
+            glitter.addAssign(hash)
 
+            // Scarcity
+            glitter.assign(glitter.pow(this.road.glitterScarcity))
+
+            // Intensity
+            glitter.mulAssign(this.road.glitterIntensity)
+            
+            const perlinUv = positionWorld.xz.mul(this.road.glitterPerlinFrequency)
+            const perlin = texture(this.game.noises.perlin, perlinUv).r
+            glitter.mulAssign(perlin)
+
+            const middle = uv().y.mul(PI).sin()
+            glitter.mulAssign(middle)
+            
+            // Output
             const baseColor = this.road.color.toVar()
-            baseColor.addAssign(max(glitterLighten, middleLighten).mul(0.2))
+            baseColor.addAssign(glitter)
 
             return vec3(baseColor)
         })()
@@ -74,9 +95,9 @@ export class Scenery
         })
         mesh.material = material
 
-        // Physics
-        this.road.body = mesh.userData.object.physical.body
-        this.road.body.setEnabled(false)
+        // // Physics
+        // this.road.body = mesh.userData.object.physical.body
+        // this.road.body.setEnabled(false)
 
         // Debug
         if(this.game.debug.active)
@@ -92,4 +113,8 @@ export class Scenery
         }
     }
 
+    update()
+    {
+        this.road.glitterVariation.value += this.game.ticker.deltaScaled * 0.004 + this.game.view.delta.length() * 0.004
+    }
 }
